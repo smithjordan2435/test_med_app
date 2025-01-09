eb46aa8426446108dd9d796a89e7cbde2b6e4706
jordan <smith.jordan2435@gmail.com>
2025-01-08 22:59:09 -0500

    mongpassword

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:vuoFhuFNOT8o91DHBVZ5wmz7@127.0.0.1:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
